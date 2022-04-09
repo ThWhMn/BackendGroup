f1ca2c6200a69895b745e037c10c97d9540d4dfe
ThWhMn <1692657550@qq.com>
2022-04-09 22:43:45 +0800

    1st week advance task

--- user-service/src/main/java/com/example/userservice/dao/UserDAO.java
+++
@@ -1,9 +0,0 @@
-package com.example.userservice.dao;
-
-import com.example.userservice.pojo.User;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface UserDAO {
-    User getUserById(String id);
-}
